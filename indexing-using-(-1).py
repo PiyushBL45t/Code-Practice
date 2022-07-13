00430af1d4e7d87ffa9f00af705dd311436529ef
Piyush <75735209+PiyushBL45t@users.noreply.github.com>
2022-07-13 11:56:10 +0530

    Create indexing-using-(-1).py

---
+++ indexing-using-(-1).py
@@ -0,0 +1,21 @@
+list_name = [element_!, element_2, ..., element_n]
+
+int_list= [3, 4, 5, 5, 6] # a list containing all integers
+char_list = ['a', 'b', 'c', 'd'] # al list of characters
+str_list = ["apple", 'banana', 'lemon', 'orange'] # a list of strings
+
+my_list = [45, 5, 33, 1, -9, 8, 76]
+print(my_list[-1]) 
+print(my_list[-2])
+print(my_list[-3])
+
+my_list = [45, 5, 33, 1, -9, 8, 76]
+my_list.reverse()
+
+my_list = [45, 5, 33, 1, -9, 8, 76]
+my_list[::-1]
+
+my_list = [45, 5, 33, 1, -9, 8, 76]
+my_list.pop(-1)
+print(my_list)
+
